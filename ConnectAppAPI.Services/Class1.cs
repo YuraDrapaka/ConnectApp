bd892b747dae46352a3c79030d1389d5e0086ec8
YuraDrapaka <91428413+YuraDrapaka@users.noreply.github.com>
2024-10-05 21:30:37 +0300

    DAL + first Repository implementation

--- ConnectAppAPI.Services/Class1.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace ConnectAppAPI.Services
-{
-    public class Class1
-    {
-
-    }
-}
---
+++ ConnectAppAPI.Services/Services/UsersService.cs
@@ -0,0 +1,47 @@
+﻿using ConnectAppAPI.DataAccess.Models;
+using ConnectAppAPI.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AppDbContextAPI.DataAccess;
+
+
+namespace ConnectAppAPI.Services.Services
+{
+    public class UsersService : IRepository<AspNetUser, AppDbContext>
+    {
+        private readonly AppDbContext _context;
+
+        public async AspNetUser CreateAsync(AppDbContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        public AspNetUser Delete(AspNetUser entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<AspNetUser> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public AspNetUser? GetByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public AspNetUser? UpdateAsync(AspNetUser entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
